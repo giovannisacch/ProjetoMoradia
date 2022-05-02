a319c3b0d4f086260a828a8a9d8dd6ff14db023f
Luiz83 <31358895+Luiz83@users.noreply.github.com>
2022-05-02 14:41:21 -0300

    Update ApartamentoTest.cs

--- ProjetoMoradia.TesteUnitario/ApartamentoTest.cs
+++ ProjetoMoradia.TesteUnitario/ApartamentoTest.cs
@@ -9,7 +9,7 @@ namespace ProjetoMoradia.TesteUnitario
         public void ApartamentoDeveriaSerDoSegundoAndar(){
             //Arrange - Preparação
             var andarEsperado = 2;
-            var apto = new Apartamento("teste", 2222, 43, 2, 1, andarEsperado, 500);
+            var apto = new Apartamento("teste", 2222, 43, 2, 1, andarEsperado, 510);
 
             //Act - Agir
             var andarApto = apto.GetAndar();
